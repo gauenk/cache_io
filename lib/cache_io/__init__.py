f7080d0eb27b928edeb2008c1a0c5193e99798e8
gauenk <kent.gauen@gmail.com>
2022-09-02 20:47:30 -0400

    with misc

--- lib/cache_io/__init__.py
+++ lib/cache_io/__init__.py
@@ -4,3 +4,4 @@ from .exp_cache import ExpCache
 from .tensor_cache import TensorCache
 from .uuid_cache import UUIDCache,compare_config
 from .mesh import mesh_pydicts,append_configs
+from .misc import strings2bools,exp_strings2bools
---
+++ lib/cache_io/misc.py
@@ -0,0 +1,14 @@
+
+
+def exp_strings2bools(exp):
+    for field in exp:
+        if not isinstance(exp[field],str): continue
+        if exp[field].lower() == "true":
+            exp[field] = True
+        elif exp[field].lower() == "false":
+            exp[field] = False
+
+def strings2bools(exps):
+    # convert "true" or "false" to True or False
+    for exp in exps:
+        exp_strings2bools(exps)
