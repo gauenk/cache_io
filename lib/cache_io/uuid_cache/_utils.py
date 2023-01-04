520a8e41ad3b84a4de4031802b3b87e32549aff3
gauenk <kent.gauen@gmail.com>
2023-01-04 14:12:43 -0500

    testing code

--- lib/cache_io/uuid_cache/_utils.py
+++ lib/cache_io/uuid_cache/_utils.py
@@ -5,11 +5,30 @@ from ._debug import VERBOSE
 
 def compare_config(existing_config,proposed_config,verbose=False):
     if isinstance(existing_config,str): return False
+    # if _dev_cmp(existing_config):
+    #     print("-"*10)
+    #     print(proposed_config)
+    #     print(existing_config)
     left_cmp = compare_pair(existing_config,proposed_config,["uuid"])
     right_cmp = compare_pair(proposed_config,existing_config,["uuid"])
     pair_cmp = left_cmp and right_cmp
+    # if _dev_cmp(existing_config):
+    #     print(left_cmp,right_cmp,pair_cmp)
     return pair_cmp
 
+# -> FREELY DELETE ME. <-
+def _dev_cmp(cfg):
+    match = True
+    fields = {"arch_name":"lidia","vid_name":"sunflower",
+              "temporal_chunk_size":0,"temporal_chunk_overlap":0.0}
+    for field in fields:
+        if not(cfg[field] == fields[field]):
+            match = False
+            break
+    if "spatial_chunk_overlap" in cfg:
+        match = False
+    return match
+            
 def compare_pair(cfg_a,cfg_b,skips,verbose=False):
     for key,value in cfg_a.items():
         if key in skips: continue
